257cc3db4f610350d5e681a6db92a8d1058e7f10
JucianKaue <jucian_decezare2015@hotmail.com>
2023-01-25 16:36:55 -0300

    Available personal stars grade ready.

--- lib/pages/client/availabepersonals.page.dart
+++ lib/pages/client/availabepersonals.page.dart
@@ -1,6 +1,52 @@
 import 'package:flutter/foundation.dart';
 import 'package:mysql1/mysql1.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+// This custom clipper help us achieve n-pointed star shape
+class StarClipper extends CustomClipper<Path> {
+  /// The number of points of the star
+  final int points;
+  StarClipper(this.points);
+
+  // Degrees to radians conversion
+  double _degreeToRadian(double deg) => deg * (math.pi / 180.0);
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    double max = 2 * math.pi;
+
+    double width = size.width;
+    double halfWidth = width / 2;
+
+    double wingRadius = halfWidth;
+    double radius = halfWidth / 2;
+
+    double degreesPerStep = _degreeToRadian(360 / points);
+    double halfDegreesPerStep = degreesPerStep / 2;
+
+    path.moveTo(width, halfWidth);
+
+    for (double step = 0; step < max; step += degreesPerStep) {
+      path.lineTo(halfWidth + wingRadius * math.cos(step),
+          halfWidth + wingRadius * math.sin(step));
+      path.lineTo(halfWidth + radius * math.cos(step + halfDegreesPerStep),
+          halfWidth + radius * math.sin(step + halfDegreesPerStep));
+    }
+
+    path.close();
+    return path;
+  }
+
+  // If the new instance represents different information than the old instance, this method will return true, otherwise it should return false.
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    StarClipper starClipper = oldClipper as StarClipper;
+    return points != starClipper.points;
+  }
+  
+}
 
 class AvailablePersonals extends StatelessWidget {
   int userID;
@@ -17,7 +63,7 @@ class AvailablePersonals extends StatelessWidget {
                     timeout: const Duration(seconds: 10)
                   )
                 );
-    var result = await conn.query("SELECT COUNT(*), user.name, user.photo_url, especialty.name_especialty, personal.description FROM user JOIN PERSONAL ON personal.user_id = user.id JOIN especialty ON especialty.id = personal.especialty_id;");
+    var result = await conn.query("SELECT COUNT(*), user.name, user.photo_url, especialty.name_especialty, personal.score, personal.description FROM user JOIN PERSONAL ON personal.user_id = user.id JOIN especialty ON especialty.id = personal.especialty_id;");
     return result;
   }
 
@@ -35,6 +81,61 @@ class AvailablePersonals extends StatelessWidget {
   //   var result = (await conn.query("SELECT name FROM user WHERE id = $userID")).elementAt(0);
   //   return result;
   // }
+
+  _buildStarsScore(double number) {
+    var decimalPart = number.toString().split('.')[1];
+
+    List<Widget> stars = [];
+    for (var i=1; i<=5; i++) {
+      if (number > i) {
+        stars.add(
+          SizedBox(
+            height: 20,
+            width: 20,
+            child: ClipPath(
+              clipper: StarClipper(5),
+              child: Container(
+                height: 150,
+                child: Row(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Container(color: Colors.amber,)
+                    ),
+                ],)
+              ),
+            ),
+          )
+        );
+      } else {
+        stars.add(
+          SizedBox(
+            width: 20,
+            height: 20,
+            child: ClipPath(
+              clipper: StarClipper(5),
+              child: Container(
+                height: 150,
+                child: Row(
+                  children: [
+                    Flexible(
+                      flex: int.parse(decimalPart),
+                      child: Container(color: Colors.amber,)
+                    ),
+                    Flexible(
+                      flex: (int.parse(decimalPart)-math.pow(10, decimalPart.length).toInt())*(-1),
+                      child: Container(color: Color.fromARGB(255, 117, 117, 117),)
+                    ),
+                ],)
+              ),
+            ),
+          )
+        );
+      }
+    }
+
+    return stars;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,19 +178,17 @@ class AvailablePersonals extends StatelessWidget {
                                 )
                               ),
                               child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children:  [
                                   // Nome
-                                  Text('${snapshot.data.elementAt(index)["name"]}', style: TextStyle(fontSize: 20)),
+                                  Text('${snapshot.data.elementAt(index)["name"].split(" ")[0]}', style: const TextStyle(fontSize: 20)),
                                   
-                                  SizedBox(
-                                    width: 10,
-                                  ),
                                   // grade
-                                  Icon(Icons.grade, size: 20),
-                                  Icon(Icons.grade, size: 20),
-                                  Icon(Icons.grade, size: 20),
-                                  Icon(Icons.grade, size: 20),
-                                  Icon(Icons.grade, size: 20),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: _buildStarsScore(4.55),
+                                  )
+                                  ,
                                 ],
                               ),
                             ),
